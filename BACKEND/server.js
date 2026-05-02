979d2fc14fb256451bd53b3645e7b1bd0a40d8fd
Devothe Uwineza <devothauwineza7@gmail.com>
2026-05-02 13:24:34 +0200

    fix cors

--- BACKEND/server.js
+++ BACKEND/server.js
@@ -10,8 +10,9 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 
-
-app.use(cors());
+app.use(cors({
+  origin: "https://kanda-shop.onrender.com"
+}));
 
 // Connect to MongoDB
 connectDB();
